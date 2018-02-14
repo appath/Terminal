ffbc31d11d76fb3c0fc59cc1b26df1f07dfd52f7
Boris241 <htc.boko@gmail.com>
2018-02-14 13:34:43 +0300

    Update inf.sh

--- inf.sh
+++ inf.sh
@@ -12,7 +12,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-source $HOME/.GitHub/color
+source $HOME/.info/color
 kernel=$(uname -rmo)
 system=$(source /etc/os-release && echo $PRETTY_NAME)
 pkgs=$(pacman -Q | wc -l)
