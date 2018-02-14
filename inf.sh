c8f5d04e40249ae8247a652cd67cc432a8778b67
Boris241 <htc.boko@gmail.com>
2018-02-14 13:01:28 +0300

    vim
    $Vim set fileformat=unix, wq!

--- inf.sh
+++ inf.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#  $Vim set fileformat=unix, wq!
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
